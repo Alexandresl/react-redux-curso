aea30b0cca3d6ae0b82a07c08bbc86f431886520
Alexandre Lima <limaalexguitar@gmail.com>
2019-08-06 23:13:39 -0300

    Componente PageHeader

--- todo-app/frontend/src/about/about.jsx
+++ todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero praesentium obcaecati quae exercitationem voluptatibus quos atque, tenetur blanditiis enim quo. Fuga neque, rerum expedita soluta nisi debitis autem accusantium reprehenderit?</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est beatae dolorem molestias magnam, omnis explicabo labore consequatur repellendus nesciunt atque vero commodi unde ipsam distinctio asperiores culpa impedit delectus maxime.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex nesciunt error veniam dolore sit ea laboriosam rem, tenetur, molestiae nemo labore id ab ullam unde alias, adipisci est aliquid incidunt?</p>
     </div>
 )
