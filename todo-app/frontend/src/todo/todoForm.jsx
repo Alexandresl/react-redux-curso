f689dc3ba229a470f432c5738e6b38a089a4eb44
Alexandre Lima <limaalexguitar@gmail.com>
2019-08-09 23:21:08 -0300

    Pesquisa de TODOs

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -12,7 +12,11 @@ export default props => (
                 value={props.description}></input>
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
+            <IconButton style="primary" icon="plus"
+                onClick={props.handleAdd} />
+            <IconButton style='info' icon='search'
+                onClick={props.handleSearch} />
+
         </Grid>
     </div>
 )
