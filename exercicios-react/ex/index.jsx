91227a58b014512c93ab1bb6ee38b6a6c3c5f80a
Alexandre Lima <limaalexguitar@gmail.com>
2019-08-11 20:14:01 -0300

    Integrando o React-Redux (Parte 1)

--- exercicios-react/ex/index.jsx
+++ exercicios-react/ex/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import { Field } from './field'
+import Field from './field'
 
 const reducers = combineReducers({
     field: () => ({ value: 'Opa' })
