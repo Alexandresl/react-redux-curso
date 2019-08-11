4f311ec115042b92fa53a08f8d77bfcc71cf3d85
Alexandre Lima <limaalexguitar@gmail.com>
2019-08-11 00:12:10 -0300

    Componente de Classe

---
+++ exercicios-react/ex/classComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'React'
+
+export default class ClassComponent extends Component {
+
+    render() {
+        return (
+            <h1>{this.props.value}</h1>
+        )
+    }
+
+}
