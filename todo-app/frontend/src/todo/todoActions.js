3a4ba8328a395d2ba6c763ea79f4c6011dfc4428
Alexandre Lima <limaalexguitar@gmail.com>
2019-08-17 23:51:01 -0300

    Marcar como Concluído/Pendente

--- todo-app/frontend/src/todo/todoActions.js
+++ todo-app/frontend/src/todo/todoActions.js
@@ -1,24 +1,38 @@
- import axios from 'axios'
+import axios from 'axios'
 
- const URL = 'http://localhost:3003/api/todos'
+const URL = 'http://localhost:3003/api/todos'
 
- export const changeDescription = event => ({
-    type: 'DESCRIPTION_CHANGED',
-    payload: event.target.value
- })
+export const changeDescription = event => ({
+   type: 'DESCRIPTION_CHANGED',
+   payload: event.target.value
+})
 
- export const search = () => {
-    const request = axios.get(`${URL}?sort=-createdAt`)
-    return {
-       type: 'TODO_SEARCHED',
-       payload: request
-    }
- }
+export const search = () => {
+   const request = axios.get(`${URL}?sort=-createdAt`)
+   return {
+      type: 'TODO_SEARCHED',
+      payload: request
+   }
+}
 
- export const add = (description) => {
-    return dispatch => {
-       axios.post(URL, {description})
-         .then(resp => dispatch({type: 'TODO_ADDED', payload: resp.data }))
+export const add = (description) => {
+   return dispatch => {
+      axios.post(URL, { description })
+         .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
          .then(resp => dispatch(search()))
-    }
- }
+   }
+}
+
+export const markAsDone = (todo) => {
+   return dispatch => {
+      axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+         .then(resp => dispatch(search()))
+   }
+}
+
+export const markAsPending = (todo) => {
+   return dispatch => {
+      axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+         .then(resp => dispatch(search()))
+   }
+}
